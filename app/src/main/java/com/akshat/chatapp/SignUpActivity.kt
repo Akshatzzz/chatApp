9a06f36e798a324264d53e8fb74be26deb9b6b7f
Akshat Chaturvedi <akshat.chaturvedi@bobble.ai>
2023-07-22 20:20:03 +0530

    sign up code flow.

--- app/src/main/java/com/akshat/chatapp/SignUpActivity.kt
+++ app/src/main/java/com/akshat/chatapp/SignUpActivity.kt
@@ -1,11 +1,68 @@
 package com.akshat.chatapp
 
+import android.app.ProgressDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.akshat.chatapp.databinding.ActivitySignUpBinding
+import com.akshat.chatapp.model.UserModel
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class SignUpActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySignUpBinding
+    lateinit var fireBaseAuth: FirebaseAuth
+    lateinit var firebaseDatabase: FirebaseDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initialize()
+
+        binding.btnSignUp.setOnClickListener {
+            binding.signUpProgress.visibility = View.VISIBLE
+            processSignUp()
+        }
+    }
+
+    private fun processSignUp() {
+        val name = binding.tvEnterName.text.toString()
+        val password = binding.etPassword.text.toString()
+        val email = binding.etEmail.text.toString()
+        if (name.isEmpty() || email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this@SignUpActivity, "Please Enter All the Fields", Toast.LENGTH_SHORT)
+                .show()
+            return
+        }
+        binding.etEmail.text?.clear()
+        binding.tvEnterName.text?.clear()
+        binding.etPassword.text?.clear()
+        fireBaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            binding.signUpProgress.visibility = View.GONE
+            if (task.isSuccessful) {
+                    val user = task.result.user?.uid?.let { it1 -> UserModel(name,email,"", it1) }
+                    user?.let {
+                        firebaseDatabase.reference.child("User").child(user.userId).setValue(user)
+                    }
+                    Toast.makeText(
+                        this@SignUpActivity, "Successfully Signed Up !!", Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    Toast.makeText(
+                        this@SignUpActivity,
+                        "Couldn't sign in returning ${task.exception?.message}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+    }
+
+    private fun initialize() {
+        fireBaseAuth = FirebaseAuth.getInstance()
+        firebaseDatabase = FirebaseDatabase.getInstance()
     }
 }
---
+++ app/src/main/java/com/akshat/chatapp/model/UserModel.kt
@@ -0,0 +1,19 @@
+package com.akshat.chatapp.model
+
+import java.security.acl.LastOwnerException
+
+data class UserModel(
+    val name: String,
+    val email: String,
+    val userId: String,
+    val profilePic: String? = null,
+    val lastMessage: String
+) {
+    constructor(name: String, email: String, lastMessage: String, userId: String) : this(
+        name,
+        email,
+        userId,
+        null,
+        lastMessage
+    )
+}
